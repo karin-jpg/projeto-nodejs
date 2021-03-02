74e94e9ac5e6157a19673b9cf5d977786b4a45e2
karin-jpg <andradkarin@gmail.com>
2021-03-02 20:24:43 -0300

    Criada pasta controller e biblioteca consign adicionada

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('Servidor ativo'))
-
-app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos com nodemon'))
