0a905425becf5e62c8b27b195f5248d54e14d11d
karin-jpg <andradkarin@gmail.com>
2021-03-03 19:23:04 -0300

    Inserido processo de criação da primeira tabela

--- infra/tabelas.js
+++ infra/tabelas.js
@@ -1,7 +1,20 @@
 class Tabelas {
     init(conexao){
-        console.log('Acesso a tabelas')
         this.conexao = conexao
+        this.criarAtendimentos()
+    }
+
+    criarAtendimentos(){
+
+        const sql = "CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(100) NOT NULL, pet varchar(50), servico varchar(50) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
+
+       this.conexao.query(sql, (erro) => {
+            if(erro){
+                console.log(erro)
+            } else{
+                console.log('Tabela de atendimentos criado com sucesso')
+            }
+       })
     }
 }
 
