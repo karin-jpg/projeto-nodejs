92c48f5a87f7f7f4ca410b280ea156c9efef9653
karin-jpg <andradkarin@gmail.com>
2021-03-03 19:45:34 -0300

    adição da biblioteca moments para tratamento de datas

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,9 +2,10 @@ const conexao = require('../infra/conexao')
 
 class Atendimento {
     adiciona(atendimento){
+
         const sql = "INSERT INTO Atendimentos SET ?"
         console.log(sql)
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro){
                console.log(erro)
             } else {
